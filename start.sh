e94c2133a91d4ad81188c017aa371a55a0178cb0
Wesspace <wdymg@hotmail.com>
2025-04-11 12:39:18 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-streamlit run painel_resultados_real.py
+
+echo "Iniciando bot.py..."
+python3 bot.py &
+
+echo "Iniciando painel_resultados_real.py..."
+streamlit run painel_resultados_real.py --server.port 10000 --server.address 0.0.0.0
