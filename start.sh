69bd98729a8d5794cf2c0697857fa5741dbdee79
Wesspace <wdymg@hotmail.com>
2025-04-12 13:34:47 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup python3 bot.py > bot.log 2>&1 &
-sleep 5
-streamlit run painel_resultados_real.py --server.port=10000
+
+# Inicia o painel Streamlit
+streamlit run painel_resultados_real.py --server.port=$PORT --server.enableCORS=false
