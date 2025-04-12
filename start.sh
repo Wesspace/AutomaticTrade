1f8662b1fc48fd82d9af722b93d1f2dbff38f0d5
Wesspace <wdymg@hotmail.com>
2025-04-12 13:49:40 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# Executa o painel Streamlit na porta que o Render define
 streamlit run painel_resultados_real.py --server.port=$PORT --server.enableCORS=false --server.enableXsrfProtection=false
