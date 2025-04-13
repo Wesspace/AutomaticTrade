2db7b134f5934326a2aef84f8c9e9bd9072e5944
Wesspace <wdymg@hotmail.com>
2025-04-13 01:42:07 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-streamlit run painel_resultados_real.py --server.port=$PORT --server.address=0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false
+echo "Iniciando painel..."
+streamlit run painel_resultados_real.py --server.port=$PORT --server.address=0.0.0.0 --server.headless=true --server.enableCORS=false --server.enableXsrfProtection=false
