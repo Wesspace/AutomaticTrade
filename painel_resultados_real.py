726ee2535e9cc203ca7948245635fb07c82a2a63
Wesspace <wdymg@hotmail.com>
2025-04-13 02:04:28 -0300

    Add files via upload

--- painel_resultados_real.py
+++ painel_resultados_real.py
@@ -1,13 +1,13 @@
+
 import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout="wide")
-
 st.title("Painel de Resultados - SmartTrader View 🚀")
 
 try:
     df = pd.read_csv("relatorio_volatile.csv")
     st.success("Relatório carregado com sucesso!")
-    st.dataframe(df.tail(10))  # Exibe as últimas 10 entradas
+    st.dataframe(df.tail(10))
 except Exception as e:
     st.error(f"Erro ao carregar relatório: {e}")
