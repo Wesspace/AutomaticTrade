11a066349a4bce9e6106e6f647c250f30077e9a4
Wesspace <wdymg@hotmail.com>
2025-04-13 01:26:49 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-streamlit run painel_resultados_real.py --server.port=$PORT --server.enableCORS=false --server.enableXsrfProtection=false
+streamlit run painel_resultados_real.py --server.port=$PORT --server.address=0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false
